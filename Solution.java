18dd8bfa955f3fe94aac7bd539fbd2f003420cb0
Kangmin Kim <coderkangkim@gmail.com>
2021-05-16 21:12:38 +0900

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -20,10 +20,12 @@ class Solution {
             this.adjList[i] = new ArrayList<Integer>();
 		}
 
+        // input 유효성 확인 - 전체 합이 0이 아닐 경우 불가능
 		if (s != 0) {
 			return -1;
 		}
 
+        // 간선 정보 adjList 에 입력
 		for (int[] edge : edges) {
             int node1 = edge[0];
             int node2 = edge[1];
@@ -36,12 +38,13 @@ class Solution {
 		return answer;
 	}
 
+    
 	public long dfs(int index) {
 		this.visited[index] = true;
-        ArrayList<Integer> adjs = this.adjList[index];
 
-		for (int i = 0; i < adjs.size(); i++) {
-			int curr = adjs.get(i);
+        // 간선 노드를 확인하며 가중치 업데이트
+		for (int i = 0; i < this.adjList[index].size(); i++) {
+			int curr = this.adjList[index].get(i);
 			if (visited[curr] == false) {
 				this.longAns[index] += dfs(curr);
 			}
